ff991cd89b97cefdb357414ec736f53ad9b9b51e
Yamisipq <rosasyamith@gmail.com>
2025-10-14 11:53:46 -0500

    actz csv

--- csv/ex_csv.py
+++ csv/ex_csv.py
@@ -1,4 +1,5 @@
 import csv
+from tabulate import tabulate
 
 list_aprendices_clave=["nombre","apellido","direccion","telefono", "ficha"]
 list_aprendices=[]
@@ -21,3 +22,16 @@ for idx, estudiante in enumerate(list_aprendices):
     print(f"\nEstudiante {idx+1}:")
     for clave, valor in estudiante.items():
         print(f"  {clave.capitalize()}: {valor}")
+
+#Escribir en un archivo CSV
+with open("aprendices.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    for aprendiz in list_aprendices:
+        writer.writerow(aprendiz)
+
+# Leer un archivo CSV como diccionarios
+with open("aprendices.csv", "r", encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+    print(tabulate(reader, headers="keys"))
+    # for row in reader:
+    #     print(f"{row['nombre']} está en la ficha {row['ficha']}, Programa {row['programa']}")
